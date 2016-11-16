bcb8599b6c2d018100fb00b777ad389ecb4d8bea
fredhorch <fred@sparkae.com>
2016-11-16 13:18:42 -0500

    Styles and access to document elements.

--- client/main.js
+++ client/main.js
@@ -8,29 +8,49 @@ Units = {
     { kwh:
       { value: "kwh",
         label: "Kilowatt hours (kWh)",
-        ratio: 1},
+        ratio: 29307.1070171944},
       btu:
       { value: "btu",
         label: "British Thermal Units (BTU)",
-        ratio: 3412.14},
+        ratio: 100000},
+      mj:
+        { value: "mj",
+          label: "megajoules (MJ)",
+          ratio: 105.5055852619},
       kj:
       { value: "kj",
         label: "Kilojoules (kJ)",
-        ratio: 3600},
+        ratio: 105505.5852619},
       J:
       { value: "J",
         label: "Joules (J)",
-        ratio: 3600000}
+        ratio: 105505585.2619},
+      therm:
+      { value: "therm",
+        label: "US Therms (therm)",
+        ratio: 1}
     },
   power:
     { watts:
       { value: "watts",
         label: "Watts",
-        ratio: 745.7 },
-      horsepower:
-      { value: "horsepower",
-        label: "Horsepower",
-        ratio: 1 }
+        ratio: 746 },
+      hpi:
+      { value: "hpi",
+        label: "Mechanical horsepower (hpI)",
+        ratio: 1.000402478 },
+      hpe:
+      { value: "hpe",
+        label: "Electrical horsepower (hpE)",
+        ratio: 1 },
+      hpm:
+      { value: "hpm",
+        label: "Metric horsepower (hpM)",
+        ratio:  1.014277727},
+        btuh:
+        { value: "btuh",
+          label: "BTU per hour (btuh)",
+          ratio: 2545.45766 }
     }
 };
 
@@ -57,8 +77,17 @@ Template.unitconverter.onCreated(function unitconverterOnCreated() {
   this.conversion = new ReactiveVar( false );
   this.convertfrom = new ReactiveVar( false );
   this.convertto = new ReactiveVar( false );
-  this.valuefrom = new ReactiveVar( 0 );
-  this.valueto = new ReactiveVar ( 0 );
+  this.valuefrom = new ReactiveVar( "" );
+  this.valueto = new ReactiveVar( "" );
+
+  this.setValue = function (stringval, ratio) {
+    const result = Math.round( parseFloat(stringval) * ratio * 100000000) / 100000000;
+    if ( isNaN(result) ) {
+      return "";
+    } else {
+      return result;
+    }
+  }
 
 });
 
@@ -87,11 +116,24 @@ Template.unitconverter.helpers({
 
 Template.unitconverter.events({
   'change [name="unittype"]'(event, instance) {
+    // console.log("Document", document);
+    console.log("Unit From value", document.getElementById("unitfrom").value);
+    console.log("Unit To value", document.getElementById("unitto").value);
     console.log("Change unittype", event.target);
+    console.log("Instance",instance);
     // set unit type
     instance.conversion.set(event.target.selectedOptions[0].value);
+    if (instance.valuefrom.get() || instance.valueto.get()) {
+      console.log("Value From", instance.valuefrom.get());
+      console.log("Value To", instance.valueto.get());
+      console.log("Conversion", instance.conversion.get());
+      instance.valuefrom.set("");
+      instance.valueto.set("");
+      document.getElementById("unitfrom").value = "";
+      document.getElementById("unitto").value = "";
+    }
   },
-  'change [name="unitfrom"]'(event, instance) {
+  'change [id="unitfrom"]'(event, instance) {
     console.log("Change unitfrom", event.target);
     const myUnitFrom = event.target.value;
     instance.convertfrom.set(myUnitFrom);
@@ -104,33 +146,33 @@ Template.unitconverter.events({
     console.log("Value From", instance.valuefrom.get());
     const myRatio = myUnitsObj[myUnitTo].ratio / myUnitsObj[myUnitFrom].ratio;
     console.log("Ratio", myRatio);
-    instance.valueto.set( parseFloat(instance.valuefrom.get()) * myRatio);
+    instance.valueto.set( instance.setValue(instance.valuefrom.get(), myRatio) );
   },
-  'input [name="quantityfrom"]'(event, instance) {
+  'input [id="quantityfrom"]'(event, instance) {
     console.log("Change quantityfrom", event.target);
     instance.valuefrom.set(event.target.value);
     const myUnitFrom = instance.convertfrom.get();
     const myUnitTo = instance.convertto.get();
     const myUnitsObj = Units[instance.conversion.get()];
     const myRatio = myUnitsObj[myUnitTo].ratio / myUnitsObj[myUnitFrom].ratio;
-    instance.valueto.set( parseFloat(instance.valuefrom.get()) * myRatio);
+    instance.valueto.set( instance.setValue(instance.valuefrom.get(), myRatio) );
   },
-  'change [name="unitto"]'(event, instance) {
+  'change [id="unitto"]'(event, instance) {
     console.log("Change unitto", event.target);
     const myUnitTo = event.target.value;
     instance.convertto.set(myUnitTo);
     const myUnitFrom = instance.convertfrom.get();
     const myUnitsObj = Units[instance.conversion.get()];
-    const myRatio = myUnitsObj[myUnitFrom].ratio / myUnitsObj[myUnitTo].ratio;
-    instance.valuefrom.set( parseFloat(instance.valueto.get()) * myRatio);
+    const myRatio = myUnitsObj[myUnitTo].ratio / myUnitsObj[myUnitFrom].ratio;
+    instance.valueto.set( instance.setValue(instance.valuefrom.get(), myRatio) );
   },
-  'input [name="quantityto"]'(event, instance) {
+  'input [id="quantityto"]'(event, instance) {
     console.log("Change quantityto", event.target);
     instance.valueto.set(event.target.value);
     const myUnitFrom = instance.convertfrom.get();
     const myUnitTo = instance.convertto.get();
     const myUnitsObj = Units[instance.conversion.get()];
     const myRatio = myUnitsObj[myUnitFrom].ratio / myUnitsObj[myUnitTo].ratio;
-    instance.valuefrom.set(  parseFloat(instance.valueto.get()) * myRatio);
+    instance.valuefrom.set( instance.setValue(instance.valueto.get(), myRatio) );
   },
 });
